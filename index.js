9e678414630db488b422f255c5beab80c1ca0da9
Ian Moffitt <IMoffitt@barkleyrei.com>
2021-02-24 11:18:42 -0500

    feat: Adjust index to accept parameters.

--- index.js
+++ index.js
@@ -1,26 +1,84 @@
 'use strict';
 
+/* Inspired by ninety-six/mono-fractal */
+
 const mandelbrot = require('@frctl/mandelbrot');
+const _ = require('lodash');
 
 /*
- * Configure the theme
+ * Required config overrides incoming options
+ * Note: styles array is merged
  */
-const subTheme = mandelbrot({
-	skin: "fuchsia",
-	styles: ['default', '/_subtheme/tweaks.css'] // link to the default stylesheet followed by a custom one
-});
+const requiredConfig = {
+	styles: [
+		'default',
+		'/recursion-theme/recur.css'
+	],
+	scripts: ['default']
+};
 
-/*
- * Specify a template directory to override any view templates
- */
-subTheme.addLoadPath(__dirname + '/views');
+module.exports = (options, mode) => {
 
-/*
- * Specify the static assets directory that contains the custom stylesheet.
- */
-subTheme.addStatic(__dirname + '/assets', '/_subtheme');
+	/*
+	* Configure the theme
+	*/
+	const styles = _.union(requiredConfig.styles, options.styles || []);
+	const scripts = _.union(requiredConfig.scripts, options.scripts || []);
 
-/*
- * Export the customised theme instance so it can be used in Fractal projects
- */
-module.exports = subTheme;
+	const config = _.defaultsDeep({
+		skin: 'black',
+		styles: styles,
+		scripts: scripts,
+		format: 'json',
+		highlightStyles: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/default.min.css',
+		information: [
+			{
+				'label': 'Last Compiled',
+				'value': new Date(),
+				'type': 'time',
+				'format': (value) => {
+					return '<br/>' + value.toLocaleString('en');
+				}
+			}
+		]
+	}, _.clone(options || {}));
+
+	config.panels = config.panels || ['notes', 'info'];
+	config.devpanels = config.devpanels || ['notes', 'html', 'info', 'context', 'resources'];
+
+	if (mode !== 'development') {
+		// Production settings
+		config.labels = {
+			panels: {
+				info: 'Preview'
+			}
+		};
+	} else {
+		// Development settings
+		config.panels = config.devpanels;
+	}
+
+	config.nav = config.nav || ['search', 'components', 'docs', 'information'];
+	config.favicon = config.favicon || '/recursion-theme/favicon.ico';
+
+	/*
+	 * Configure the theme
+	 */
+	const subTheme = mandelbrot(config);
+
+	/*
+	 * Specify a template directory to override any view templates
+	 */
+	subTheme.addLoadPath(__dirname + '/views');
+
+	/*
+	 * Specify the static assets directory that contains the custom stylesheet.
+	 */
+	subTheme.addStatic(__dirname + '/assets', '/recursion-theme');
+
+	/*
+	 * Export the customised theme instance so it can be used in Fractal projects
+	 */
+	return subTheme;
+
+};
